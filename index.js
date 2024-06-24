76c9946395a001b852f44772b3311f618ac58364
Frm2003 <felipperamos75@gmail.com>
2024-06-24 12:12:44 -0300

    correções

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ytdl = require('ytdl-core')
 const cors = require('cors')
+const { PassThrough } = require('stream');
 const app = express();
 
 app.use(express.json())
@@ -17,19 +18,27 @@ app.post('/teste', async (req, res) => {
     try {
         const { videoUrl } = req.body;
 
-        const options = { quality: 'highestaudio', filter: 'audioonly' };
+        if (!videoUrl) {
+            return res.status(400).send('URL do vídeo não fornecida.');
+        }
+
+        const options = {
+            quality: 'highestaudio',
+            filter: 'audioonly'
+        };
+
         const info = await ytdl.getInfo(videoUrl);
         const fileName = `${info.videoDetails.title}.mp3`;
 
         res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
         res.setHeader('Content-Type', 'audio/mpeg');
 
-        const videoStream = ytdl(videoUrl, options);
-
         req.socket.setTimeout(10 * 60 * 1000);
 
+        const videoStream = ytdl(videoUrl, options);
+
         res.once('finish', () => {
-            videoStream.destroy(); 
+            videoStream.destroy();
         });
 
         const passThrough = new PassThrough();
@@ -41,11 +50,11 @@ app.post('/teste', async (req, res) => {
         });
 
     } catch (error) {
-        console.error('Erro durante o download:', error);
-        res.status(500).send('Erro durante o download do vídeo.');
+        console.error('Erro durante o processamento:', error);
+        res.status(500).send('Erro durante o processamento do vídeo.');
     }
 });
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(8000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
